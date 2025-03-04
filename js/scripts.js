406f778cb327687dc2c965a25f71fcb760ebb7dd
JaceTappen <28JTappen@live.ytech.edu>
2025-03-04 09:58:41 -0500

    I added stuff like links and numbers
    I added links and colors

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// Change Variable
+let stylemode = document.cookie;
+
+// Change Variable
+let stylelink = document.getElementById('style');
+
+// Conditional Branch with Else Branch
+if (stylemode == 'dark') {
+    // Change the href property of the link
+    stylelink.href = 'css/dark.css';
+} else {
+    stylelink.href = 'css/light.css';
+    document.cookie = 'light';
+}
+
+// Common Event
+function switchmode() {
+    //Conditional Statement with Else Branch
+    if (stylemode == 'light') {
+        stylemode = 'dark';
+        stylelink.href = 'css/dark.css';
+        document.cookie = 'dark';
+    } else {
+        stylemode = 'light';
+        stylelink.href = 'css/light.css';
+        document.cookie = 'light';
+    }
+}
+
+function showhide(params) {
+    let toolbarbuttons = document.getElementById('buttons');
+    if (toolbarbuttons.style.display == 'block') {
+        toolbarbuttons.style.display = 'none';
+    } else {
+        toolbarbuttons.style.display = 'block';
+    }
+}
